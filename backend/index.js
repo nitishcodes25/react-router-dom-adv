c6e7c9bd470ec594ece469450674b41ad8f8d276
nitishcodes25 <nitish.codes25@gmail.com>
2025-02-27 10:33:29 +0530

    connected database with backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 import express from "express"
 import dotenv from "dotenv"
 import eventRouter from './routes/event.route.js'
+import mongoose from "mongoose"
 const app = express()
 const PORT = process.env.PORT || 3000
 dotenv.config()
+
+mongoose.connect(process.env.MONGO_DB_URL)
+.then(()=>{
+    console.log("Connected to DB")
+})
+.catch((err)=>{
+    console.log('err',err)
+})
 
 app.use('/event',eventRouter)
 
