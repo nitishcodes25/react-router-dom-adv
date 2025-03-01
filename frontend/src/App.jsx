5054a722b7061af2daeff32aad5ee4feb3172b5e
nitishcodes25 <nitish.codes25@gmail.com>
2025-03-01 21:50:26 +0530

    lazy loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/RootLayout.jsx";
 import Home from "./pages/Home.jsx";
@@ -8,14 +8,20 @@ import EventLayout from "./pages/EventLayout.jsx";
 import NewEvent from "./pages/NewEvent.jsx";
 import EventDetail, {
   loader as eventDetailLoader,
-  action as deleteActionEvent
+  action as deleteActionEvent,
 } from "./pages/EventDetail.jsx";
 import EditEvent from "./pages/EditEvent.jsx";
-import {action as addEventAction} from './components/EventForm.jsx'
-import NewsletterPage,{action as newsletterAction} from "./pages/NewsletterPage.jsx";
-import AuthenticationPage,{action as authAction} from "./pages/AuthenticationPage.jsx";
-import {action as logoutAction} from './pages/Logout.jsx'
+import { action as addEventAction } from "./components/EventForm.jsx";
+import NewsletterPage, {
+  action as newsletterAction,
+} from "./pages/NewsletterPage.jsx";
+// import AuthenticationPage, {
+//   action as authAction,
+// } from "./pages/AuthenticationPage.jsx";
+import { action as logoutAction } from "./pages/Logout.jsx";
 import { checkAuthLoader, tokenLoader } from "./utils/storage.js";
+
+const AuthenticationPage = lazy(() => import("./pages/AuthenticationPage.jsx"));
 
 export default function App() {
   const router = createBrowserRouter([
@@ -23,13 +29,28 @@ export default function App() {
       path: "/",
       element: <RootLayout />,
       errorElement: <ErrorPage />,
-      id: 'root',
+      id: "root",
       loader: tokenLoader,
       children: [
         { index: true, element: <Home /> },
-        {path:"newsletter", element: <NewsletterPage /> , action: newsletterAction},
-        {path:"auth", element: <AuthenticationPage />, action: authAction},
-        {path:"logout", action: logoutAction},
+        {
+          path: "newsletter",
+          element: <NewsletterPage />,
+          action: newsletterAction,
+        },
+        {
+          path: "auth",
+          element: (
+            <Suspense fallback={<p>loading...</p>}>
+              <AuthenticationPage />
+            </Suspense>
+          ),
+          action: (meta) =>
+            import("./pages/AuthenticationPage.jsx").then((module) =>
+              module.action(meta)
+            ),
+        },
+        { path: "logout", action: logoutAction },
         {
           path: "event",
           element: <EventLayout />,
@@ -37,14 +58,28 @@ export default function App() {
             { index: true, element: <Events />, loader: addEventLoader },
             {
               path: ":eventId",
-              id: 'event-detail',
+              id: "event-detail",
               loader: eventDetailLoader,
               children: [
-                { index: true, element: <EventDetail /> , action: deleteActionEvent},
-                { path: "edit", element: <EditEvent /> ,action : addEventAction, loader: checkAuthLoader},
+                {
+                  index: true,
+                  element: <EventDetail />,
+                  action: deleteActionEvent,
+                },
+                {
+                  path: "edit",
+                  element: <EditEvent />,
+                  action: addEventAction,
+                  loader: checkAuthLoader,
+                },
               ],
             },
-            { path: "newevent", element: <NewEvent /> , action: addEventAction, loader:  checkAuthLoader},
+            {
+              path: "newevent",
+              element: <NewEvent />,
+              action: addEventAction,
+              loader: checkAuthLoader,
+            },
           ],
         },
       ],
